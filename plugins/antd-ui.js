566a1a4983f9ac01a0dbb9c05a641b5eeb4c9eeb
Dreamer5201314 <1085011285@qq.com>
2023-04-13 21:30:48 +0800

    fix bugs

--- plugins/antd-ui.js
+++ plugins/antd-ui.js
@@ -2,7 +2,6 @@ import Antd from 'ant-design-vue/lib'
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue';
 
 Vue.use(Antd)
 Vue.use(ElementUI);
