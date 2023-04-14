a38954ea0275ec83671c5326680f297f0726969b
Dreamer5201314 <1085011285@qq.com>
2023-04-14 10:30:00 +0800

    add toggle button and change some style of UMLUserCase.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
+<!--    <div class="toggle-button" @click="toggleCollapse" style="height: 50px;width: 550px"><i id="fold" class="el-icon-s-fold"></i></div>-->
     <el-menu
       style="position: fixed;top: 0;left: 0;height: 100%;"
+      :width="isCollapse?'64px':'200px'"
       default-active="1"
+      :collapse-transition="false"
+      :collapse="isCollapse"
       class="el-menu-vertical-demo"
       @open="handleOpen"
       @close="handleClose"
@@ -10,23 +14,26 @@
       text-color="#fff"
       active-text-color="#ffd04b"
     >
-      <el-menu-item index="1" @click="change('/mainPage/FP')">
+      <el-menu-item index="1" @click="toggleCollapse">
+        <i id="fold" class="el-icon-s-fold"></i>
+      </el-menu-item>
+      <el-menu-item index="2" @click="change('/mainPage/FP')">
         <i class="el-icon-s-home"></i>
         <span slot="title">功能点度量</span>
       </el-menu-item>
-      <el-menu-item index="2" @click="change('/mainPage/UMLClass')">
+      <el-menu-item index="3" @click="change('/mainPage/UMLClass')">
         <i class="el-icon-s-promotion"></i>
         <span slot="title">类图辅助度量</span>
       </el-menu-item>
-      <el-menu-item index="3" @click="change('/mainPage/UMLUserCase')">
+      <el-menu-item index="4" @click="change('/mainPage/UMLUserCase')">
         <i class="el-icon-s-opportunity"></i>
         <span slot="title">用例图辅助度量</span>
       </el-menu-item>
-      <el-menu-item index="4" @click="change('/mainPage/DataFlow')">
+      <el-menu-item index="5" @click="change('/mainPage/DataFlow')">
         <i class="el-icon-share"></i>
         <span slot="title">控制流图辅助度量</span>
       </el-menu-item>
-      <el-menu-item index="5" @click="change('/mainPage/AST')">
+      <el-menu-item index="6" @click="change('/mainPage/AST')">
         <i class="el-icon-paperclip"></i>
         <span slot="title">代码辅助度量</span>
       </el-menu-item>
@@ -42,13 +49,28 @@
 export default {
   data(){
     return{
-      current:['/mainPage/FP']
+      current:['/mainPage/FP'],
+      isCollapse: false
     }
   },
 
   methods: {
     change(dir){
       this.$router.push(dir)
+    },
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
+      if(this.isCollapse === true){
+        //document.getElementById("fold").setAttribute("class","el-icon-s-unfold")
+        document.getElementById("fold").className = "el-icon-s-unfold"
+        //alert(this.isCollapse)
+      }
+      else
+      {
+        //document.getElementById("fold").setAttribute("class","el-icon-s-fold")
+        document.getElementById("fold").className = "el-icon-s-fold"
+        //alert(this.isCollapse)
+      }
     },
     handleOpen(key, keyPath) {
       console.log(key, keyPath);
@@ -59,3 +81,17 @@ export default {
   }
 }
 </script>
+<style>
+.toggle-button:hover {
+  background: rgb(67, 74, 80);
+}
+.toggle-button{
+  background: #545c64;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2rem;
+  cursor: pointer;
+}
+</style>
